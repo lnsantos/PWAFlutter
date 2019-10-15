e367a9918d811a38f0ebd230a2a65c4ca94a6ce7
Donizete Vida <donizetevida@Donizetes-MacBook-Pro.local>
2019-10-14 22:15:27 -0300

    New image icon

--- android/app/src/main/kotlin/com/solvian/webapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/solvian/webapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app
+package com.solvian.webapp
 
 import android.os.Bundle
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
 
-void main() {
-  runApp(MeuApp());
+void main() => runApp(MeuApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  InAppWebViewController webView;
+  String url = "";
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: InAppWebView(
+                    initialUrl: "https://webapp.smartfield.homolog.solvian.com",
+                    initialHeaders: {
+
+                    },
+                    initialOptions: {
+
+                    },
+                    onWebViewCreated: (InAppWebViewController controller) {
+                      webView = controller;
+                    },
+                    onLoadStart: (InAppWebViewController controller, String url) {
+                      print("started $url");
+                      setState(() {
+                        this.url = url;
+                      });
+                    },
+                    onProgressChanged: (InAppWebViewController controller, int progress) {
+                      setState(() {
+                        this.progress = progress/100;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ].where((Object o) => o != null).toList(),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MeuApp extends StatelessWidget {
